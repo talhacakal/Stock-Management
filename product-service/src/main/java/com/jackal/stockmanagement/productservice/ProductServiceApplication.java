062709e87560bd5610b27846908a879ebfb4fc43
Talha Çakal <65471143+talhacakal@users.noreply.github.com>
2023-10-12 20:30:26 +0300

    product service eureka client config settings

--- product-service/src/main/java/com/jackal/stockmanagement/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/jackal/stockmanagement/productservice/ProductServiceApplication.java
@@ -1,9 +1,11 @@
 package com.jackal.stockmanagement.productservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
